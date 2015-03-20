4d81e3e66c5b207250696133da4c1dd9baadda99
sylvain garaud <sgaraud@galactus.site>
2015-03-20 15:20:49 +0100

    add ipv6 support

--- extension.js
+++ extension.js
@@ -67,7 +67,11 @@ const IpMenu = new Lang.Class({
    _getNetworkDevices: function(nmc) {
       this.devices = nmc.get_devices();
       for each (let device in this.devices) {
+
          this._updateIp(device);
+         //log(NetworkManager.DeviceState.UNMANAGED);
+         //log(device.get_iface());
+         //log(device.get_state());
          device.connect('state-changed',Lang.bind(this,this._updateIp));
       }
    },
@@ -76,8 +80,33 @@ const IpMenu = new Lang.Class({
       this.actor.show();
    },
 
-   _getIps: function (dhcpconf,iface) {
-      mapIps.set(iface,dhcpconf.get_one_option("ip_address"));
+   _decodeIp4: function(num) {
+      num = num>>>0;
+      let array = Uint8Array(4);
+      array[0] = num;
+      array[1] = num >> 8;
+      array[2] = num >> 16;
+      array[3] = num >> 24;
+
+      return array[0]+'.'+array[1]+'.'+array[2]+'.'+array[3];
+   },
+
+   _decodeIp6: function(num) {
+      return num;
+   },
+
+
+   _getIp4s: function (ip4conf,iface) {
+      mapIps.set(iface,this._decodeIp4(ip4conf));
+      if (deviceSelected == null){
+         deviceSelected = iface;
+         this.label.set_text(mapIps.get(deviceSelected));
+      }
+      this._createPopupMenu();
+   },
+
+   _getIp6s: function (ip6conf,iface) {
+      mapIps.set(iface,this._decodeIp6(ip6conf));
       if (deviceSelected == null){
          deviceSelected = iface;
          this.label.set_text(mapIps.get(deviceSelected));
@@ -87,18 +116,42 @@ const IpMenu = new Lang.Class({
 
    _updateIp: function(dev) {
       let iface = dev.get_iface();
-      let dhcpconf = dev.get_dhcp4_config();
-      if (dhcpconf != null) {
-         if (dhcpconf.get_one_option("ip_address") == undefined ) {
-            dhcpconf.connect('notify::options',Lang.bind(this,function(){
-               this._getIps(dhcpconf,iface);
+      let ip4conf = dev.get_ip4_config();
+      let ip6conf = dev.get_ip6_config();
+
+      if (ip4conf != null) {
+         log("ip4");
+         log(ip4conf.get_addresses()[0]);
+
+         if (ip4conf.get_addresses()[0].get_address() == undefined ) {
+            ip4conf.connect('notify::addresses',Lang.bind(this,function(){
+               log('callback ip4 addresses');
+               this._getIp4s(ip4conf.get_addresses()[0].get_address(),iface);
             }));
          }
          else {
-            this._getIps(dhcpconf,iface);
+            this._getIp4s(ip4conf.get_addresses()[0].get_address(),iface);
          }
       }
+      else if (ip6conf != null) {
+         log("ip6");
+         log(ip6conf.get_addresses()[0]);
+
+         if (ip6conf.get_addresses()[0].get_address() == undefined ) {
+            ip6conf.connect('notify::addresses',Lang.bind(this,function(){
+               log('callback ip6 addresses');
+               this._getIp6s(ip6conf.get_addresses()[0].get_address(),iface);
+            }));
+         }
+         else {
+            this._getIp6s(ip6conf.get_addresses()[0].get_address(),iface);
+         }
+
+
+      }
       else{
+         log('delete interface');
+         log(iface);
          mapIps.delete(iface);
 
          if(deviceSelected == iface){
@@ -110,7 +163,6 @@ const IpMenu = new Lang.Class({
                for (let key of mapIps.keys()) {
                   deviceSelected = key;
                   this.label.set_text(mapIps.get(deviceSelected));
-                  return
                }
             }
          }
