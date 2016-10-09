e19c897e2c9e15c3ca83f01028215f8e1dc7834c
sylvain garaud <garaud@gmail.com>
2016-10-09 21:30:13 +0200

    add script to generate strings to translate

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 echo "Compile schema"
 glib-compile-schemas ./schemas/
 
+echo "Compile French locale (po to mo)"
+msgfmt locale/fr/show-ip.po -o locale/fr/LC_MESSAGES/show-ip.mo
+
 echo "Create zip archive"
 rm show-ip@sgaraud.github.com.zip
 zip -r show-ip@sgaraud.github.com.zip . -x \*.git* \*.idea*
---
+++ translate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#Extract strings to translate .pot
+xgettext -k_ -kN_ -o locale/show-ip.pot *.js
+
+#Create a new french translation po
+msginit -i locale/show-ip.pot -o locale/fr/show-ip.po
