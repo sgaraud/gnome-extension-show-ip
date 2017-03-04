c6ea6af84df947dabd9c2ce547cdb283e7e455a1
Paul Wise <pabs3@bonedaddy.net>
2017-03-04 13:24:02 +0800

    Avoid turning extra local *.js files into xgettext options
    Suggested-by: shellcheck

--- translate.sh
+++ translate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Extract strings to translate .pot
-xgettext -k_ -kN_ -o locale/show-ip.pot *.js
+xgettext -k_ -kN_ -o locale/show-ip.pot ./*.js
 
 #Create a new French translation po
 msginit -i locale/show-ip.pot -o locale/fr/show-ip.po
