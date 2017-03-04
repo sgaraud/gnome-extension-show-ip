2d5ed6d129ada4418bb3ac099a35aa086846d928
Paul Wise <pabs3@bonedaddy.net>
2017-03-04 13:02:05 +0800

    Correct words with incorrect case
    IP, GNOME, GObject, French had some incorrect case.
    
    Several sentences also had some incorrect case.
    
    Suggested-by: spellintian --picky

--- extension.js
+++ extension.js
@@ -1,22 +1,22 @@
 /*
- * Show Ip menu gnome extension
+ * Show IP menu GNOME extension
  * https://github.com/sgaraud/gnome-extension-show-ip
  * 
  * Copyright (C) 2015 Sylvain Garaud
  *
- * This file is part of show-ip gnome extension.
- * Show Ip gnome extension is free software: you can redistribute it and/or modify
+ * This file is part of Show-IP GNOME extension.
+ * Show IP GNOME extension is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Show Ip gnome extension is distributed in the hope that it will be useful,
+ * Show IP GNOME extension is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Show Ip gnome extension.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Show IP GNOME extension.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
@@ -40,7 +40,7 @@ const _ = Gettext.gettext;
 const Soup = imports.gi.Soup;
 const _httpSession = new Soup.SessionAsync();
 /* This makes the session work under a proxy. The funky syntax here
- * is required because of another libsoup quirk, where there's a gobject
+ * is required because of another libsoup quirk, where there's a GObject
  * property called 'add-feature', designed as a construct property for
  * C convenience.
  */
--- translate.sh
+++ translate.sh
@@ -3,5 +3,5 @@
 #Extract strings to translate .pot
 xgettext -k_ -kN_ -o locale/show-ip.pot *.js
 
-#Create a new french translation po
+#Create a new French translation po
 msginit -i locale/show-ip.pot -o locale/fr/show-ip.po
